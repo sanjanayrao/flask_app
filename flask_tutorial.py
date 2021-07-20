cdb55708b6b19e72ab8659c4a411bf4c15925b6d
Sanjana Rao <sanjanayrao@gmail.com>
2021-07-20 10:57:23 -0500

    smol website

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -4,6 +4,9 @@ import json
 
 app = Flask(__name__)
 user_admin = False
+cat_image = requests.get("https://aws.random.cat/meow?ref=apilist.fun")
+dog_image = requests.get("https://random.dog/woof.json?ref=apilist.fun")
+
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -20,13 +23,19 @@ def login():
 
 @app.route("/<usr>", methods=["POST", "GET"])
 def user(usr):
-    cat_image = requests.get("https://aws.random.cat/meow?ref=apilist.fun")
+    global cat_image
+    global dog_image
     if request.method == "POST":
         if request.form.get("cat"):
             cat_image = requests.get("https://aws.random.cat/meow?ref=apilist.fun")
-    cat_image = json.loads(cat_image.text)
-    cat_image = cat_image['file']
-    return render_template("usr.html", user=usr, cat_image=cat_image)
+            cat_image = json.loads(cat_image.text)
+            cat_image = cat_image['file']
+        elif request.form.get("dog"):
+            dog_image = requests.get("https://random.dog/woof.json?ref=apilist.fun")
+            dog_image = json.loads(dog_image.text)
+            dog_image = dog_image['url']
+
+    return render_template("usr.html", user=usr, cat_image=cat_image, dog_image=dog_image)
 
 if __name__ == "__main__":
     app.run(debug=True)
